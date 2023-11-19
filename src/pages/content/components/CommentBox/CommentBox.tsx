801a5aa00fa9319656c2ecf3687ecfb73d72fb3b
MichaelXerxes <hiszpan4444@googlemail.com>
2023-11-19 21:56:53 +0000

    CommentBox styling

--- src/pages/content/components/CommentBox/CommentBox.tsx
+++ src/pages/content/components/CommentBox/CommentBox.tsx
@@ -6,14 +6,14 @@ interface Props {
 }
 const CommentBox: React.FC<Props> = ({ comment, commentAuthor, rateStars }) => {
   return (
-    <div className=" flex  w-[410px] h-[110px] flex-col pl-4 pr-5 rounded-lg bg-stone-200 mt-3 mb-3">
+    <div className=" flex  w-[410px] h-[120px] flex-col pl-4 pr-5 rounded-lg bg-gray-100 mt-3 mb-3">
       <div className=" flex flex-row w-[390px] h-6 justify-between mt-2 mb-3">
         <span className=" text-base text-zinc-900 ">{commentAuthor}</span>
         <div className=" flex flex-row  h-10 mr-4">* * * * * {rateStars}</div>
       </div>
-      <div className="max-w-sm break-all line-clamp-3 ">
+      <div className="max-w-sm break-words line-clamp-3 text-sm text-gray-400">
         This is a long comment that will be truncated after three lines, showing
-        an ellipsis if it exceeds the maximum width and line limit... This is a
+        an ellipsis if it exceeds the maximum width and line limit. This is a
         long comment that will be truncated after three lines, showing an
         ellipsis if it exceeds the maximum width and line limit...
       </div>
--- src/pages/content/components/TabsContent/Comments/Comments.tsx
+++ src/pages/content/components/TabsContent/Comments/Comments.tsx
@@ -3,7 +3,7 @@ import CommentBox from "../../CommentBox/CommentBox";
 interface Props {}
 const Comments: React.FC<Props> = ({}) => {
   return (
-    <div className="flex  flex-col items-center w-full justify-center  bg-slate-500 ">
+    <div className="flex  flex-col items-center w-full justify-center ">
       <CommentBox commentAuthor="Michael" rateStars={5} comment="hhhhh" />
       <CommentBox commentAuthor="Michael" rateStars={5} comment="hhhhh" />
       <CommentBox commentAuthor="Michael" rateStars={5} comment="hhhhh" />
